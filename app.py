a39a635abeec9265ae7cae8ee1a446f78902076c
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-08-02 17:08:52 +0100

    bug fixed

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ pio.templates.default = "plotly_white"
 # create app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 
 # # load data
