a79ccdd1be402334ec2073abfcde11a423e8395c
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-08-02 16:34:06 +0100

    deploy simple

--- app.py
+++ app.py
@@ -15,25 +15,26 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-# load data
-dir_name = "data"
-morocco_clusters = pd.DataFrame(columns=["lat", "lon", "city_name", "year"," cluster"])
+# # load data
+# dir_name = "data"
+# morocco_clusters = pd.DataFrame(columns=["lat", "lon", "city_name", "year"," cluster"])
 
-for filename in os.listdir(dir_name):
-    path = os.path.join(dir_name, filename)
-    morocco_clusters = morocco_clusters.append(pd.read_csv(path), ignore_index=True)
+# for filename in os.listdir(dir_name):
+#     path = os.path.join(dir_name, filename)
+#     morocco_clusters = morocco_clusters.append(pd.read_csv(path), ignore_index=True)
 
-# process data
-morocco_clusters = morocco_clusters.dropna(axis=1)
-morocco_clusters = morocco_clusters.sort_values(by=["year"])
+# # process data
+# morocco_clusters = morocco_clusters.dropna(axis=1)
+# morocco_clusters = morocco_clusters.sort_values(by=["year"])
 
+morocco_clusters = pd.DataFrame({ 'lat': [1, 2, 3, 4], 'lon': [1, 2, 3, 4], 'cluster': [1, 0, 0, 1], 'year': [1, 0, 1, 0]})
 
 # construct graph
 fig = px.scatter_geo(
     morocco_clusters, 
     lat="lat", lon="lon",
-    color="cluster", color_discrete_sequence=['#FFA15A', '#636EFA', "#00CC96"],
-    hover_name="city_name", 
+    color="cluster", # color_discrete_sequence=['#FFA15A', '#636EFA', "#00CC96"],
+    # hover_name="city_name", 
     animation_frame="year"
     )
 
